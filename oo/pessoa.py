6d0ad89ff8e5b348ae6f839ffd6c257fbc375f08
Diego Maranhão <diegomaranhao@outlook.com>
2020-05-11 10:22:18 -0400

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=34):
+    def __init__(self, *filhos, nome=None, idade=34):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('DOM')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Diego'
-    print(p.nome)
+    miguel = Pessoa(nome='Miguel')
+    dom = Pessoa(miguel, nome='DOM')
+    print(Pessoa.cumprimentar(dom))
+    print(id(dom))
+    print(dom.cumprimentar())
+    print(dom.nome)
+    for filho in dom.filhos:
+        print(filho.nome)
