99f0f5587f7f4d3e8a6999bc63b005192379614f
Diego Maranhão <diegomaranhao@outlook.com>
2020-05-11 10:17:00 -0400

    Criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=34):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('DOM')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Diego'
+    print(p.nome)
